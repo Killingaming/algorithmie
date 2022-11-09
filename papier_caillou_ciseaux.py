a89150d1618f86ee856b187ff26b1844392ac4a9
Killingaming <justinkillingamer@gmail.com>
2022-11-09 15:02:19 +0100

    update papier_etc

--- papier_caillou_ciseaux.py
+++ papier_caillou_ciseaux.py
@@ -1,10 +1,21 @@
 DEBUT
-#On admet une fonction input qui admet l'entrée d'un joueur
-# On définit une table avec 3 éléments : "papier" "caillou" "ciseaux"
+"A REMETTRE EN ORDRE!!"
+# On définit une table "elements" avec 3 éléments : "papier" "caillou" "ciseaux"
 #On définit les valeurs 0, 1 et 2
 
 # on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
 
+
+#On définit la fonction jeu
+    # on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
+    #On admet une fonction input qui récupère l'entrée d'un joueur quand on l'exécute (choixJoueur1) (choixJoueur2)
+    #On admet une fonction 
+
+
+"#On définit une fonction jeu qui permet de mettre le choix "
+
+
+input()
 
 
 papier = 0
