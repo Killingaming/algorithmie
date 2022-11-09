a4ff9f35ae76629b5d7a3ec98aa034bcf223f170
Killingaming <justinkillingamer@gmail.com>
2022-11-09 14:31:01 +0100

    papier_etc

---
+++ papier_caillou_ciseaux.py
@@ -0,0 +1,14 @@
+DEBUT
+#On admet une fonction input qui admet l'entrée d'un joueur
+# On définit une table avec 3 éléments : "papier" "caillou" "ciseaux"
+#On définit les valeurs 0, 1 et 2
+
+# on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
+
+
+
+papier = 0
+caillou = 1
+ciseaux = 2
+
+
