2775ce2af1758b451eb1e81887029d7f77f8304a
Killingaming <justinkillingamer@gmail.com>
2022-11-14 12:37:33 +0100

    Update papier_caillou_ciseaux.py

--- papier_caillou_ciseaux.py
+++ papier_caillou_ciseaux.py
@@ -1,15 +1,38 @@
 DEBUT
-"A REMETTRE EN ORDRE!!"
-# On définit une table "elements" avec 3 éléments : "papier" "caillou" "ciseaux"
+"A REMETTRE EN ORDRE!!" "les crochets sont pour créer des tableaux"
+# On définit une table "elements" avec 3 éléments : "papier"=0 "caillou"=1 "ciseaux"=2
+#on définit 
 #On définit les valeurs 0, 1 et 2
 
 # on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
 
 
 #On définit la fonction jeu
-    # on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
-    #On admet une fonction input qui récupère l'entrée d'un joueur quand on l'exécute (choixJoueur1) (choixJoueur2)
-    #On admet une fonction 
+    
+    "#On admet une fonction input qui récupère l'entrée d'un joueur quand on l'exécute (choixJoueur1) 
+    "rename" #Assigne a choixJoueur le retour de l'execution de la fonction input
+    #Si x est supérieur a 3
+        #Alors j'affiche le message "Yoyoyo"
+
+    #Tant que x est True
+        #Alors j'execute la fonction michel
+
+    
+    ----> PAS BON : #pierre = 0
+    ----> #On assigne a pierre la Valeur 0
+    
+   
+   
+   
+   
+    #On admet une fonction random qui donneras au hasard des valeurs entre 0 et 2 (Ordinateur)
+    
+    
+    
+    #Si les 2 choix sont egaux
+        #Retourner "No Win!"
+
+    #Sinon le joueur 1 
 
 
 "#On définit une fonction jeu qui permet de mettre le choix "
