3383a9e2c849ed7d31084df67c748ab9186d11f4
Killingaming <115621519+Killingaming@users.noreply.github.com>
2022-11-17 10:12:02 +0100

    Add files via upload
    Changing the pseudo code  of the rock paper scisors

--- papier_caillou_ciseaux.py
+++ papier_caillou_ciseaux.py
@@ -1,46 +1,58 @@
-#DEBUT
-"A REMETTRE EN ORDRE!!" "les crochets sont pour créer des tableaux"
-# On définit une table "elements" avec 3 éléments : "papier"=0 "caillou"=1 "ciseaux"=2
-#On 
+DEBUT
+"A REMETTRE EN ORDRE!!"
+# On définit une table "elements" avec 3 éléments : "papier = 0" "caillou = 1" "ciseaux = 2"
+#On Initialise papier en 0
+#On Initialise caillou en 1 
+#On Initialise ciseaux en 2
+ 
+les valeurs 0, 1 et 2, 
 
-# on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
+#on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
 
+#on définit la fonction end() 
 
 #On définit la fonction jeu
+#TANT que TRUE   -> while TRUE
+    #manche = 0
+        # on admet une fonction random qui donneras au hasard des valeurs entre 0 et 2
+        #On admet une fonction input qui récupère l'entrée d'un joueur quand on l'exécute (choixJoueur1) (Adversaire)
+        #On admet une fonction qui renvoie un résultat de partie:
+            #si choixJoueur1 == 0
+                #si Adversaire == 0
+                    #print "Draw"
+                #sinon Adversaire == 1
+                    #print "You Win"
+                #sinon si Adversaire == 2
+                    #print "You Lose"
+
+            #sinon choixJoueur1 == 1 
+                #si Adversaire == 0
+                    #print "You Lose"
+                #sinon Adversaire == 1 
+                    #print "Draw"
+                #sinon si Adversaire == 2
+                    #print "You Win"
+        
+            #sinon si choixJoueur1 == 2
+                #si Adversaire == 0
+                    #print "You Win"
+                #sinon Adversaire == 1
+                    #print "You Lose"
+                #sinon si Adversaire == 2 le 
+                    #print "Draw"
+            
+    #Ajouter 1 à manche 
+    #Afficher le nombre de manches faites
+
+#FIN
     
-    #On admet une fonction input qui récupère l'entrée d'un joueur quand on l'exécute (choixJoueur1) 
-    #Assigne a choixJoueur le retour de l'execution de la fonction input
-    #Si x est supérieur a 3
-        #Alors j'affiche le message "Yoyoyo"
-
-    #Tant que x est True
-        #Alors j'execute la fonction michel
-
-    
-    ----> PAS BON : #pierre = 0
-    ----> #On assigne a pierre la Valeur 0
-    
-   
-   
-   
-   
-    #On admet une fonction random qui donneras au hasard des valeurs entre 0 et 2 (Ordinateur)
-    
-    
-    
-    #Si les 2 choix sont egaux
-        #Retourner "No Win!"
-
-    #Sinon le joueur 1 
+        
+        
+        
+        
+        
+        
 
 
-#"On définit une fonction jeu qui permet de mettre le choix "
-
-
-
-
-papier = 0
-caillou = 1
-ciseaux = 2
-
+input()
 
