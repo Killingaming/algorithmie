72828aa473d44d7f5da0779548a411811f055ac7
Killingaming <justinkillingamer@gmail.com>
2022-11-17 19:58:03 +0100

    update tictactoe

--- tictactoe_pseudo_code.py
+++ tictactoe_pseudo_code.py
@@ -10,7 +10,7 @@
         #Initialisation du tableau de jeu (vide) gamePlate, attribut de l'objet TicTacToe
         #Initialisation du pointOwner pour le premier joueur, attribut de l'objet TicTacToe (on part du principe qu'en Player VS IA le joueur commence)
             
-    #On définit la méthode affiche avec comme paramètre content qui affichera le tableau du jeu
+    #On définit la méthode affiche avec comme paramètre une liste content qui affichera le tableau du jeu en console
         # pour i allant de 0 à la longueur du tableau content -1
             #On initialise la variable contentLine à une chaine de caractères vide
             #pour j allant de 0 à la longueur du tableau content -1
@@ -18,14 +18,14 @@
                 #On ajoute à contentLine la chaine de caractères " | "
             #On affiche en console contentLine
 
-    #On définit la méthode ticTacToeStart qui créera le tableau. 
+    #On définit la méthode ticTacToeStart qui ne prend pas de paramètres qui créera le tableau. Elle ne prend pas de paramètres.
         #pour i allant de 0 à 2 
             #on ajoute un tableau vide à la fin de l'attribut gamePlate
             #pour une variable quelconque allant de 0 à 2
                 #On rajoute un caractère dans le tableau d'index i, montrant la case n
         #on affiche le gamePlate
     
-    #on définit la méthode win qui annonce le vainqueur de la partie et prend comme paramètre la chaine de caractères pointOwner (qui vaut "X" ou "O")
+    #On définit la méthode win avec comme paramètre pointOwner qui annonce le vainqueur de la partie et prend comme paramètre la chaine de caractères pointOwner (qui vaut "X" ou "O")
         #si l'IA est présente :
             #alors
             #si pointOwner est égale à "X" :
@@ -44,18 +44,27 @@
     
    
    
-    #On définit la méthode draw qui ne prends pas de paramètres qui annonce une égalité 
+    #On définit la méthode draw qui ne prend pas de paramètre qui annonce une égalité s'il y en a une 
         #On renvoie qu'il s'agit est une égalité 
 
 
 
-#On définit la méthode playerTurn qui seras utile pour les actions à faire du joueur
+    #On définit la méthode playerTurn qui ne prends pas de paramètres qui seras utile pour les actions à faire du joueur
+        #On définit la variable rowCoord qui est le résultat entier d'un input qui demande au joueur un nombre entre 1 et 3, nombre auquel on retire 1 (pour les index)
+        #On vérifie que rowCoord est supérieure ou égale à 0 et dans le même temps inférieure ou égale à 2, si ce n'est pas le cas on renvoie un message expliquant l'origine de l'erreur
+        #On définit la variable colCoord qui est le résultat entier d'un input qui demande au joueur un nombre entre 1 et 3, nombre auquel on retire 1 (pour les index)
+        #On vérifie que colCoord est supérieure ou égale à 0 et dans le même temps inférieure ou égale à 2, si ce n'est pas le cas on renvoie un message expliquant l'origine de l'erreur
+        #On retourne le tuple contenant rowCoord et colCoord
 
+    #On définit la méthode botTurn qui ne prends pas de mparamètres, qui seras utile pour les actions à faire du bot
+        #On définit la variable botRowCoord qui choisira au hasard un résultat entre 0 et 2
+        #On définit la variable 
 
-    
-
-
-            #retourner
-
+    #On définit la méthode playerWin qui prend un paramètre player et renvoie si ce dernier a gagné
+        #On définit une variable win égale à None
+        #On définit une variable length qui est égale à la longueur du tableau attribut gamePlate (le nombre de lignes)
+        #pour i allant de 0 à length-1 
+            #On indente win à True
+            #pour j allant de 0 à length-1 
 
 #FIN
---
+++ titactoe.py
@@ -0,0 +1,225 @@
+import random  #importation de la bibiliothèque random 
+class TicTacToe (object):  #création de la class TicTacToe de type object 
+
+    def __init__(self,secondPlayer=False): #surcharge de la méthode constructeur __init__
+        if secondPlayer :  #s'il y a un second joueur
+            self._iaMode = False   #Pas de présence d'une IA
+        else : #sinon, il n'y a qu'un seul joueur
+            self._iaMode = True #on indique la présence d'une IA
+        #self._winningCombos = [[(0,0),(0,1),(0,2)],[(0,0),(1,0),(2,0)],[(0,0),(1,1),(2,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)],[(0,1),(1,1),(2,1)],[(0,2),(1,2),(2,2)],[(0,2),(1,1),(2,0)]]   #Les combos gagnants
+        self._gamePlate=[]
+        self._pointOwner = "X"
+
+    def affiche(self,content):  #affichage du tableau de jeu
+        for i in range(len(content)):
+            contentLine = ""
+            for j in range(len(content)):
+                contentLine += content[i][j]
+                contentLine += (" | ")
+            print(contentLine)  
+
+    
+    def ticTacToeStart(self):   #Création du tableau 
+        for i in range(3):
+            self._gamePlate.append([])
+            for _ in range (3) :
+                self._gamePlate[i].append("*") #caractère montrant la case n                                                                                                                     on choisi auparavant
+        self.affiche(self._gamePlate) 
+        
+    
+    def win(self,pointOwner): #annonces des vainqueurs
+        if self._iaMode :
+            if pointOwner == "X" :
+                print("Player Wins !")  #retourner le joueur  a gagné
+            else :
+                print("AI wins !") #retourner l'IA a gagné
+        else :
+            if pointOwner == "X" :
+                print("Player 1 wins !")  #retourner le joueur 1 a gagné
+            else :
+                print("Player 2 wins !") #retourner le joueur 2 a gagné
+
+    def draw(self):
+        print("That's a draw !")   
+
+    def playerTurn(self):   #Les actions à faire du joueur 
+        rowCoord=int(input("Please select a row number between 1 and 3 : "))-1
+        assert rowCoord >= 0 and rowCoord <=2, "You're supposed to have chosen a number between 1 and 3..."
+        colCoord=int(input("Please select a column number between 1 and 3 : "))-1
+        assert colCoord >= 0 and colCoord <=2, "You're supposed to have chosen a number between 1 and 3..."
+        return (rowCoord, colCoord)  #retoune la ligne et la colonne du point
+
+    def botTurn(self) :   #Les actions à faire du bot
+        botRowCoord=random.randint(0,2)   #le robot choisi une coordonnée horizontale aléatoire
+        botColCoord=random.randint(0,2)   #le robot choisi une coordonnée verticale aléatoire
+        return (botRowCoord, botColCoord) #retourne la ligne et la colonne du point
+
+    def playerWin(self, player):
+        win = None
+
+        length = len(self._gamePlate)
+
+        # on vérifie les lignes
+        for i in range(length):
+            win = True
+            for j in range(length):
+                if self._gamePlate[i][j] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # on vérifie les colonnes
+        for i in range(length):
+            win = True
+            for j in range(length):
+                if self._gamePlate[j][i] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # con vérifie les diagonales
+        win = True
+        for i in range(length):
+            if self._gamePlate[i][i] != player:
+                win = False
+                break
+        if win:
+            return win
+
+        win = True
+        for i in range(length):
+            if self._gamePlate[i][length - 1 - i] != player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+    def swapTurn(self, player):
+        return 'X' if player == 'O' else 'O'
+    
+    def filledGamePlate(self):
+        filled = True
+        for i in range (len(self._gamePlate)) :
+            for j in range (len(self._gamePlate)) :
+                if self._gamePlate[i][j] == "*" :
+                    filled = False
+                    break
+        if filled:
+            return filled
+        return False
+        
+
+    def ticTacToeGame(self):    #Le jeu commence 
+        takenCoords=[]   #on initialise takenCoords qui enregistrons  les coordonées dans une liste vide 
+        continuing=True   
+        if self._iaMode :
+            while continuing : #tant que personne ne gagne
+                  
+                self._playerCoords=self.playerTurn()
+                if self._gamePlate[self._playerCoords[0]][self._playerCoords[1]] != "*" and self._playerCoords in takenCoords:
+                    print("You cannot pick this used space... try again")
+                    self._playerCoords=self.playerTurn()
+                else :
+                    takenCoords.append(self._playerCoords)
+                
+                self._gamePlate[self._playerCoords[0]].insert(self._playerCoords[1],"X")    
+                self._gamePlate[self._playerCoords[0]].pop(self._playerCoords[1]+1)
+                
+                if self.playerWin(self._pointOwner) :
+                        continuing = False
+                        self.win(self._pointOwner)
+                        break
+                self._pointOwner = self.swapTurn(self._pointOwner)
+
+                self._botCoords=self.botTurn()
+                while self._gamePlate[self._botCoords[0]][self._botCoords[1]] != "*" :
+                    self._botCoords=self.botTurn()
+                #else :
+                takenCoords.append(self._botCoords)
+                
+                print("AI played ",(self._botCoords[0]+1,self._botCoords[1]+1))
+                
+                self._gamePlate[self._botCoords[0]].insert(self._botCoords[1],"O")
+                self._gamePlate[self._botCoords[0]].pop(self._botCoords[1]+1)
+                print(self._gamePlate)
+                self.affiche(self._gamePlate)
+                 
+                if self.playerWin(self._pointOwner) :
+                    continuing = False
+                    self.win(self._pointOwner)
+                    break
+                self._pointOwner = self.swapTurn(self._pointOwner)
+                
+                assert len(takenCoords) <= 9, "An internal error occured..."
+                if self.filledGamePlate() :                                  
+                    continuing = False
+                    self.draw()
+                    break
+                
+        else :
+            while continuing :
+            
+                self._firstPlayerCoords=self.playerTurn()
+                if self._gamePlate[self._firstPlayerCoords[0]][self._firstPlayerCoords[1]] != "*" and self._firstPlayerCoords in takenCoords:
+                    print("You cannot pick this used space... try again")
+                    self._firstPlayerCoords=self.playerTurn()
+                else :
+                    takenCoords.append(self._firstPlayerCoords)
+
+                self._gamePlate[self._firstPlayerCoords[0]].insert(self._firstPlayerCoords[1],self._pointOwner)    
+                self._gamePlate[self._firstPlayerCoords[0]].pop(self._firstPlayerCoords[1]+1)
+                self.affiche(self._gamePlate)
+                
+                if self.playerWin(self._pointOwner) :
+                    continuing = False
+                    self.win(self._pointOwner)
+                    break
+                
+                if self.filledGamePlate() :                                  
+                    continuing = False
+                    self.draw()
+                    break
+
+                self._pointOwner=self.swapTurn(self._pointOwner)
+
+                self._secondPlayerCoords=self.playerTurn()
+                if self._gamePlate[self._secondPlayerCoords[0]][self._secondPlayerCoords[1]] != "*" and self._secondPlayerCoords in takenCoords:
+                    print("You cannot pick this used space... try again")
+                    self._secondPlayerCoords=self.playerTurn()
+                else :
+                    takenCoords.append(self._secondPlayerCoords)
+            
+                self._gamePlate[self._secondPlayerCoords[0]].insert(self._secondPlayerCoords[1],self._pointOwner)    
+                self._gamePlate[self._secondPlayerCoords[0]].pop(self._secondPlayerCoords[1]+1)
+                self.affiche(self._gamePlate)
+
+                
+                
+                print(self._gamePlate)
+                if self.playerWin(self._pointOwner) :
+                    continuing = False
+                    self.win(self._pointOwner)
+                    break
+                
+                if self.filledGamePlate() :                                  
+                    continuing = False
+                    self.draw()
+                    break
+
+                self._pointOwner = self.swapTurn(self._pointOwner)
+                
+                
+
+
+
+
+
+
+
+
+game=TicTacToe()
+game.ticTacToeStart()
+game.ticTacToeGame()
